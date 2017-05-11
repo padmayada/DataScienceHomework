78bf0adffb257aaea851ebe7c637eb161feb6423
padmayada <padmayada@live.com>
2017-05-11 15:40:42 -0700

    Add files via upload

---
+++ DS350Assignment4.R
@@ -0,0 +1,81 @@
+##--------------------------------------------
+##
+## Chicago Diabetes Homework (Lecture 4)
+## Date: 5/10/2017
+## Name: Padma Yada
+## Class: PCE Data Science Methods Class
+##
+##--------------------------------------------
+
+
+##-----Load Libraries-----
+require(logging)
+
+# Declare the loading data function
+load_data = function(datafile, logger=NA){
+  data = read.csv(datafile, stringsAsFactors=FALSE)
+  loginfo("Loaded Data.", logger="data_logger")
+  
+  # Check if any data was loaded
+  if(nrow(data)==0){
+    logwarn("No Data Loaded", logger="data_logger")
+  }
+  return(data)
+}
+
+## Get the slope and intercept
+slope_intercept = function(lm_model){
+ lm_model_intercept = summary(lm_model)$coefficients[1]
+ lm_model_slope = summary(lm_model)$coefficients[2]
+ lm_model_data = c(lm_model_intercept, lm_model_slope)
+ return(lm_model_data)
+}
+
+test_slope_intercept = function(){
+  test_data_x = c(1,2,3)
+  test_data_y = c(3,5,7)
+  test_output = slope_intercept(lm(test_data_y ~ test_data_x))
+  expected_output = c(1, 2)
+  stopifnot(test_output == expected_output)
+}
+
+if (interactive()){
+  # Run unit test:
+   test_slope_intercept
+  
+  # Set logging information
+  basicConfig()
+  addHandler(writeToFile, logger="data_logger", file="file_log.log")
+  
+  # Set working director and load data
+  loginfo("Setting wd and loading data.", logger="data_logger")
+  setwd('C:/Machine learning/UW - Data Science/')
+  data = read.csv('ChicagoDiabetesData.csv', stringsAsFactors = FALSE)
+  
+  # Number of Hospitalizations vs. Crude Admittance Rate.
+  data_means = apply(data[-1],2,mean)
+  
+  hospitalizations = data_means[grepl('Hospitalizations', names(data[-1]))]
+  admit_rate = data_means[grepl('Crude.Rate.[0-9]+$', names(data[-1]), perl = TRUE)]
+   
+  plot(hospitalizations, admit_rate)
+  grid()
+  best_line = lm(admit_rate ~ hospitalizations)
+  abline(best_line, col="gray23", lty=2)
+  a1 <- slope_intercept(best_line)
+  
+  loginfo((paste('For Number of Hospitalizations vs. Crude Admittance Rate, The intercept is', a1[1],'and the slope is',a1[2])))
+  
+  # Change in Number of Hospitalizations vs. Change in Crude Admittance Rate
+  
+  hospitalizations_diff = diff(hospitalizations)
+  admit_rate_diff = diff(admit_rate)
+  
+  plot(hospitalizations_diff, admit_rate_diff)
+  grid()
+  best_line_diff = lm(admit_rate_diff ~ hospitalizations_diff)
+  abline(best_line_diff, col="gray23", lty=2)
+  
+  a2 <- slope_intercept(best_line_diff)
+  loginfo((paste('For Change in Number of Hospitalizations vs. Change in Crude Admittance Rate, The intercept is', a2[1],'and the slope is',a2[2])))
+}
